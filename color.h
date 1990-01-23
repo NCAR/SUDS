d90bf3c051bfe3154bf72ff5de3b3b973868317c
Chris Burghart <burghart@ucar.edu>
1990-01-23 09:05:04 +0000

    Added three new colors and changed color macros from "C_TRACEx" to "C_DATAx"
    git-svn-id: http://svn.eol.ucar.edu/svn/rdss/trunk/suds@232 3f9b6c58-9310-455a-aec6-8b30269ab333

--- color.h
+++ color.h
@@ -1,7 +1,7 @@
 /*
  * Color definitions and color table
  *
- * $Revision: 1.3 $ $Date: 1989-08-23 10:46:31 $ $Author: burghart $
+ * $Revision: 1.4 $ $Date: 1990-01-23 09:05:04 $ $Author: burghart $
  * 
  */
 # ifndef var
@@ -21,17 +21,20 @@
 # define C_BG2		(3 + Colorbase)
 # define C_BG3		(4 + Colorbase)
 # define C_BG4		(5 + Colorbase)
-# define C_TRACE1	(6 + Colorbase)
-# define C_TRACE2	(7 + Colorbase)
-# define C_TRACE3	(8 + Colorbase)
-# define C_TRACE4	(9 + Colorbase)
-# define C_TRACE5	(10 + Colorbase)
-# define C_TRACE6	(11 + Colorbase)
+# define C_DATA1	(6 + Colorbase)
+# define C_DATA2	(7 + Colorbase)
+# define C_DATA3	(8 + Colorbase)
+# define C_DATA4	(9 + Colorbase)
+# define C_DATA5	(10 + Colorbase)
+# define C_DATA6	(11 + Colorbase)
+# define C_DATA7	(12 + Colorbase)
+# define C_DATA8	(13 + Colorbase)
+# define C_DATA9	(14 + Colorbase)
 
 /*
  * Color count (change this if another color is added above)
  */
-# define NCOLORS	12
+# define NCOLORS	15
 
 /*
  * The base for our color allocation
