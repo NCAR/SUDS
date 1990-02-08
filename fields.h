2e8123437dfe6c72d4ed85f2d68d2c1e9fa34327
Chris Burghart <burghart@ucar.edu>
1990-02-08 16:14:00 +0000

    Added fields f_range and f_azimuth
    git-svn-id: http://svn.eol.ucar.edu/svn/rdss/trunk/suds@256 3f9b6c58-9310-455a-aec6-8b30269ab333

--- fields.h
+++ fields.h
@@ -1,19 +1,19 @@
 /*
  * Fields information
  *
- * $Revision: 1.3 $ $Date: 1990-01-23 09:18:17 $ $Author: burghart $
+ * $Revision: 1.4 $ $Date: 1990-02-08 16:14:00 $ $Author: burghart $
  */
 typedef enum FLDTYPE		fldtype;
 
 enum FLDTYPE
 {
-/* 0 */		f_null,	f_time,	f_lat,	f_lon,	f_alt,
-/* 5 */		f_pres,	f_temp,	f_dp,	f_rh,	f_wspd,
-/* 10 */	f_wdir,	f_u_wind,	f_v_wind,	f_theta,f_theta_e,
-/* 15 */	f_mr,	f_qpres,	f_qtemp,	f_qrh,	f_qu,
-/* 20 */	f_qv,	f_qwind,	f_rtype,
+/* 0 */		f_null, f_time, f_lat, f_lon, f_alt,
+/* 5 */		f_pres, f_temp, f_dp, f_rh, f_wspd,
+/* 10 */	f_wdir, f_u_wind, f_v_wind, f_theta, f_theta_e,
+/* 15 */	f_mr, f_qpres, f_qtemp, f_qrh, f_qu,
+/* 20 */	f_qv, f_qwind, f_rtype, f_range, f_azimuth
 };
-# define TOTAL_FLDS 23
+# define TOTAL_FLDS 25
 
 /*
  * forward declarations
