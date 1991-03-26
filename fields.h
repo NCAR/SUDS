9f791d49cc75bd21eb5f822f6e21541cfd471249
Chris Burghart <burghart@ucar.edu>
1991-03-26 21:02:02 +0000

    Added prototypes for fd_bot () and fd_top ()
    git-svn-id: http://svn.eol.ucar.edu/svn/rdss/trunk/suds@738 3f9b6c58-9310-455a-aec6-8b30269ab333

--- fields.h
+++ fields.h
@@ -1,34 +1,40 @@
 /*
  * Fields information
  *
- * $Revision: 1.7 $ $Date: 1990-11-02 16:15:17 $ $Author: burghart $
+ * $Revision: 1.8 $ $Date: 1991-03-26 21:02:02 $ $Author: burghart $
  */
-typedef enum FLDTYPE		fldtype;
 
-enum FLDTYPE
+typedef enum
 {
 /* 0 */		f_null, f_time, f_lat, f_lon, f_alt,
 /* 5 */		f_pres, f_temp, f_dp, f_rh, f_wspd,
 /* 10 */	f_wdir, f_u_wind, f_v_wind, f_theta, f_theta_e,
 /* 15 */	f_mr, f_qpres, f_qtemp, f_qrh, f_qu,
 /* 20 */	f_qv, f_qwind, f_rtype, f_range, f_azimuth,
-/* 25 */	f_u_prime, f_v_prime, f_dz, f_vt
-};
+/* 25 */	f_u_prime, f_v_prime, f_ascent, f_vt
+} fldtype;
+
 # define TOTAL_FLDS 29
 
 /*
- * forward declarations
+ * prototypes
  */
-# ifdef VMS
-	fldtype	fd_num (char *fname);
-	char	*fd_name (fldtype fld);
-	char	*fd_desc (fldtype fld);
-	char	*fd_units (fldtype fld);
-	float	fd_center (fldtype fld), fd_step (fldtype fld);
+# ifdef __STDC__
+	fldtype	fd_num (char *);
+	char	*fd_name (fldtype);
+	char	*fd_desc (fldtype);
+	char	*fd_units (fldtype);
+	float	fd_bot (fldtype);
+	float	fd_top (fldtype);
+	float	fd_center (fldtype);
+	float	fd_step (fldtype);
 # else
 	fldtype fd_num ();
 	char	*fd_name ();
 	char	*fd_desc ();
 	char	*fd_units ();
-	float	fd_center (), fd_step ();
+	float	fd_bot ();
+	float	fd_top ();
+	float	fd_center ();
+	float	fd_step ();
 # endif
