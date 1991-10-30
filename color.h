5a0304fd1c93c94efba902e472c99d29df071caf
Chris Burghart <burghart@ucar.edu>
1991-10-30 21:34:35 +0000

    Copyright stuff
    git-svn-id: http://svn.eol.ucar.edu/svn/rdss/trunk/suds@1056 3f9b6c58-9310-455a-aec6-8b30269ab333

--- color.h
+++ color.h
@@ -1,9 +1,27 @@
 /*
  * Color definitions and color table
- *
- * $Revision: 1.4 $ $Date: 1990-01-23 09:05:04 $ $Author: burghart $
- * 
  */
+/*
+ *		Copyright (C) 1988-91 by UCAR
+ *	University Corporation for Atmospheric Research
+ *		   All rights reserved
+ *
+ * No part of this work covered by the copyrights herein may be reproduced
+ * or used in any form or by any means -- graphic, electronic, or mechanical,
+ * including photocopying, recording, taping, or information storage and
+ * retrieval systems -- without permission of the copyright owner.
+ * 
+ * This software and any accompanying written materials are provided "as is"
+ * without warranty of any kind.  UCAR expressly disclaims all warranties of
+ * any kind, either express or implied, including but not limited to the
+ * implied warranties of merchantibility and fitness for a particular purpose.
+ * UCAR does not indemnify any infringement of copyright, patent, or trademark
+ * through use or modification of this software.  UCAR does not provide 
+ * maintenance or updates for its software.
+ *
+ * $Id: color.h,v 1.5 1991-10-30 21:34:35 burghart Exp $
+ */
+
 # ifndef var
 #	ifdef VMS
 #		define var globalref
