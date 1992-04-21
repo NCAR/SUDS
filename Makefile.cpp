0fc5e123af5ce40d87389c116caea925b1b9ab95
Chris Burghart <burghart@ucar.edu>
1992-04-21 14:31:53 +0000

    Took out static linking for now because it currently breaks mixed C/Fortran programs
    git-svn-id: http://svn.eol.ucar.edu/svn/rdss/trunk/suds@1414 3f9b6c58-9310-455a-aec6-8b30269ab333

--- Makefile.cpp
+++ Makefile.cpp
@@ -1,5 +1,5 @@
 TOP = ..
-LOCALCFLAGS = -Bstatic $(XINCLUDE) $(NETCDFINCLUDE) -DLOADFILE=\"$(RDSSLIBRARIES)/suds.lf\" -DHELPDIR=\"$(ROOT)/suds/help\" $(NETCDFFLAG) 
+LOCALCFLAGS = $(XINCLUDE) $(NETCDFINCLUDE) -DLOADFILE=\"$(RDSSLIBRARIES)/suds.lf\" -DHELPDIR=\"$(ROOT)/suds/help\" $(NETCDFFLAG) 
 
 OBJS =	suds.o analyze.o cape.o class.o color.o contour.o \
 	convert.o edit.o eformat.o \
